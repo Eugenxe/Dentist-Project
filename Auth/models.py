4227fb6a581c59dc72ae420986bb8ec6eeef8e91
Eugenxe <magetoeugene567@gmail.com>
2026-03-10 14:32:29 +0300

    Created a sign up model

--- Auth/models.py
+++ Auth/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class login(Models.model):
     Username = models.CharField
     Password = models.CharField
+
+class signup(Models.model):
+    Full_name = models.CharField
