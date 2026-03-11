3a080cdc07ab5b535ac64adca10dd33b0cf1c530
Eugenxe <magetoeugene567@gmail.com>
2026-03-11 15:07:29 +0300

    Views

--- Auth/views.py
+++ Auth/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Registration
+from .serializers import registration_serializer
+from rest_framework import generics
 
 # Create your views here.
+class registation_view(generics.CreateAPIView):
+    queryset = Registration.objects.all()
+    serializer_class = registration_serializer
+
+
+
