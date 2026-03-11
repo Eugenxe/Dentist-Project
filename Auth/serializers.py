d163c01eb700c988937b87e3c46a36ee0b16cfa8
Eugenxe <magetoeugene567@gmail.com>
2026-03-11 15:06:28 +0300

    Serializer

--- Auth/serializers.py
+++ Auth/serializers.py
@@ -1,1 +1,10 @@
 from rest_framework import serializers 
+from .models import Registration
+
+class registration_serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Registration
+        fields = ['First_name', 'Second_name', 'Email']
+
+    
+ 
