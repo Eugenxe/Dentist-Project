2768c90a92662aeb4cc1bae35ba492a4767f5288
Eugenxe <magetoeugene567@gmail.com>
2026-03-13 15:46:46 +0300

    feat: Appointment views

--- Auth/views.py
+++ Auth/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from .models import Registration
-from .serializers import registration_serializer
+from .models import Registration, Appointment
+from .serializers import registration_serializer, Appointment_serializer
 from rest_framework import generics
 
 # Create your views here.
-class registation_view(generics.CreateAPIView):
+class registation_view(generics.ListCreateAPIView):
     queryset = Registration.objects.all()
     serializer_class = registration_serializer
 
-
+class Appointment_view(generics.ListCreateAPIView):
+    queryset = Appointment.objects.all()
+    serializer_class = Appointment_serializer
 
