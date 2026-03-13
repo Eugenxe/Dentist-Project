695a85ca9f4200184868e893fcd57e79235b574d
Eugenxe <magetoeugene567@gmail.com>
2026-03-13 15:46:14 +0300

    feat: Appointment url

--- Auth/urls.py
+++ Auth/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
-from .views import registation_view
+from .views import registation_view, Appointment_view
 
-urlpatterns = [path("register/", registation_view.as_view(), name = "Registration")]
+urlpatterns = [path("register/", registation_view.as_view(), name = "Registration"),
+               path("appointment/",Appointment_view.as_view(), name = "Appointment")]
